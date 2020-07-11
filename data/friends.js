2100bbe8c0c0d858b49dacf296e446880b151b31
jadibrahim99 <jadintx@gmail.com>
2020-07-10 19:44:15 -0500

    home and survey page finished with bootstrap elements, friends list added into data folder

---
+++ data/friends.js
@@ -0,0 +1,37 @@
+var listOfFriends = [
+    { 
+    "name":"St. Francis",
+    "photo":"https://www.monasteryicons.com/images/large/st-francis-icon-411.jpg",
+    "scores":[
+        5,
+        1,
+        4,
+        4,
+        5,
+        1,
+        2,
+        5,
+        4,
+        1
+      ]
+  },
+ {
+    "name":"Rasputin",
+    "photo":"https://upload.wikimedia.org/wikipedia/commons/thumb/7/71/Rasputin_PA.jpg/220px-Rasputin_PA.jpg",
+    "scores":[
+        1,
+        1,
+        3,
+        1,
+        4,
+        1,
+        5,
+        5,
+        4,
+        5
+      ]
+  }
+]
+
+
+module.exports = listOfFriends;
